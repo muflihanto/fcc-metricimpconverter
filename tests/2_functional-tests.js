dc1a2825168ff718363d30a0897dadaa5d7d16fc
muflihanto <muflihanto@gmail.com>
2023-11-20 20:35:02 +0700

    test: Convert a valid input such as 10L: GET request to /api/convert.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,7 +6,26 @@ const server = require("../server");
 chai.use(chaiHttp);
 
 suite("Functional Tests", function () {
+  this.timeout(5000);
   // TODO: Convert a valid input such as 10L: GET request to /api/convert.
+  test("Convert a valid input such as 10L: GET request to /api/convert.", function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get("/api/convert?input=10L")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        const result = JSON.parse(res.text);
+        assert.deepEqual(result, {
+          initNum: 10,
+          initUnit: "L",
+          returnNum: 2.6417,
+          returnUnit: "gal",
+          string: "10 liters converts to 2.6417 gallons",
+        });
+        done();
+      });
+  });
   // TODO: Convert an invalid input such as 32g: GET request to /api/convert.
   // TODO: Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert.
   // TODO: Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert.
